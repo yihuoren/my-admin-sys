2a094aa3188774abad58cb12c3a805afd00e738c
yihuoren <18539912827@163.com>
2018-10-10 09:57:34 +0800

    增加列表搜索功能

--- src/components/page/BatchManagement.vue
+++ src/components/page/BatchManagement.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="batch-management">
+        <div class="handle-box">
+            <el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>
+            <el-input v-model="select_title" placeholder="请输入文章标题" class="handle-input mr10"></el-input>
+            <el-input v-model="select_time" placeholder="请输入时间" class="handle-input mr10"></el-input>
+            <el-button type="primary" icon="search" @click="search">搜索</el-button>
+        </div>
         <el-table
-            :data="tableData"
+            :data="data"
             border
+            class="table"
+            @selection-change="handleSelectionChange"
             style="width: 100%">
             <el-table-column
             prop="title"
@@ -42,13 +50,31 @@
 export default {
     data() {
       return {
+        tableData: [],
         cur_page: 1,
         pageCount:0,
-        tableData: []
+        multipleSelection: [],
+        select_word: '',
+        select_title: '',
+        select_time: '',
+        is_search: false
       }
     },
-    mounted:function(){
+    created() {
         this.getData();
+    },
+    // mounted:function(){
+    //     this.getData();
+    // },
+    computed: { //计算属性，通过查询返回更新tableData
+        data() {
+            return this.tableData.filter((d)=>{
+                // 单个属性查询及模糊查询
+                if(d.title.indexOf(this.select_title) > -1 && d.timespan.indexOf(this.select_time) > -1 && (d.title.indexOf(this.select_word) > -1 || d.timespan.indexOf(this.select_word) > -1)){
+                    return d;
+                }
+            })
+        }
     },
     methods:{
         // 分页导航
@@ -66,7 +92,23 @@ export default {
                     _this.pageCount=_data.totalCount;
                     _this.tableData=_data.data;
                 })
+        },
+        search() {
+            this.is_search = true;
+        },
+        handleSelectionChange(val) {
+            this.multipleSelection = val;
         }
     }
 }
 </script>
+<style scoped>
+    .handle-box {
+        margin-bottom: 20px;
+    }
+    .handle-input {
+        width: 300px;
+        display: inline-block;
+    }
+</style>
+
